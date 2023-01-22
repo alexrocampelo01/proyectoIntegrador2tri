232e2429bcbc2f7372a43f1edf8e577a0f2599b6
Alejandro Rodriguez Campelo <alejandrorodriguezcampelo@gmail.com>
2023-01-22 19:28:13 +0100

    js trabajando en la rejojida de datos

---
+++ codigo/js/cargadorComponetes.js
@@ -0,0 +1,13 @@
+fetch('../html/cabecera.html')
+    .then( (response)=> response.text())
+    .then( (html) => {
+        let cabecera =document.querySelector('#cabecera');
+        cabecera.innerHTML = html;
+    });
+fetch('../html/pie.html')
+    .then((response) => response.text())
+    .then((html) => {
+        console.log(html);
+        let pie = document.querySelector('#pie');
+        pie.innerHTML= html;
+    });
