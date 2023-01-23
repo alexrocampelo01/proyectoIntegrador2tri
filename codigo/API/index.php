547582f0ee18fba6a2bfda38968e6d7d1abe3b2e
Alejandro Rodriguez Campelo <alejandrorodriguezcampelo@gmail.com>
2023-01-23 12:31:04 +0100

    cabios sobre entrega 2

---
+++ codigo/API/index.php
@@ -0,0 +1,5 @@
+<?php
+    if($_SERVER['REQUEST_METOD'] = 'POST'){
+        if(isset($_POST))
+    }
+?>
--- codigo/js/recolectorDeDatos.js
+++ codigo/js/recolectorDeDatos.js
@@ -2,33 +2,40 @@
 /*el funcionamien to de pulsar el boton*/
 let butRegistro = document.querySelector('#registro');
 butRegistro.addEventListener('click',recoger);
+///* parde comprovacion de campos vacios */
+// document.querySelector('#passRepe').addEventListener('blur',coincidentes);//ponemos el evento a las contraseñas
+// /*localizamos la div de errores y la guardamos en el codigo*/
+// let outputErrores = document.querySelector('#errores');
+/*array con los datos del formulario*/
+let datosUsu =[];
 function recoger(){
-    let inputnom = document.querySelector('#nom');
-    let nom = inputnom.value;
-    console.log(nom);
-    let inputApel = document.querySelector('#apel');
-    let apel = inputApel.value;
-    console.log(apel);
-    let inputnomUsu = document.querySelector('#nomUsu');
-    let nomUsu = inputnomUsu.value;
-    console.log(nomUsu);
-    let inputCorreo = document.querySelector('#correo');
-    let correo = inputCorreo.value;
-    console.log(correo);
-    let inputPass = document.querySelector('#pass');
-    let pass = inputPass.value;
-    console.log(pass);
-    let inputPassRe = document.querySelector('#passRepe');
-    let passRe = inputPassRe.value;
-    console.log(passRe);
-    let inputAltura = document.querySelector('#altura');
-    let altura = inputAltura.value;
-    console.log(altura);
-    let inputPeso = document.querySelector('#peso');
-    let peso = inputPeso.value;
-    console.log(peso);
-    let inputFechNac= document.querySelector('#altura');
-    let fechNac = inputFechNac.value;
-    console.log(fechNac);
+     
+    datosUsu['nombre'] = document.querySelector('#nom').value;
+    datosUsu['apellidos'] =  document.querySelector('#apel').value;
+    datosUsu['nombreUsu'] = document.querySelector('#nomUsu').value;
+    datosUsu['correo'] = document.querySelector('#correo').value;
+    datosUsu['pass'] = document.querySelector('#pass').value;
+    datosUsu['passRe']  = document.querySelector('#passRepe').value;
+    datosUsu['altura'] = document.querySelector('#altura').value;
+    datosUsu['peso'] = document.querySelector('#peso').value;
+    datosUsu['fechNac'] = document.querySelector('#fechNac').value;
+
+    console.log(datosUsu);
 }
+function coincidentes(){
+    console.log(document.querySelector('#pass').value);
+    console.log(document.querySelector('#passRepe').value);
+    outputErrores.innerHTML="bien";
+    if(document.querySelector('#pass').value == "" && document.querySelector('#passRepe').value == "") {
+        outputErrores.innerHTML="rellena la contraseñas";
+    }else if(!document.querySelector('#pass').value) {
+        outputErrores.innerHTML="contaseña vacio";
+    }else if(!document.querySelector('#passRepe').value){
+        outputErrores.innerHTML="contaseña repetir vacia";
+    }
+    else if(document.querySelector('#pass').value == document.querySelector('#passRepe').value){
+        outputErrores.innerHTML="bien";
+    }else{
+        outputErrores.innerHTML="no he echo nada";
+    }
 /*nombre */
